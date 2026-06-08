971595a7d8be593b64cc4055977057750d1d5d3c
ADHIRAI19 <sivakumaradhirai@gmail.com>
2026-06-08 18:35:26 +0530

    add dsa task file

---
+++ java programs/task dsa.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // DSA task placeholder
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) {
+            System.out.println("Ready for DSA input");
+            return;
+        }
+        int n = sc.nextInt();
+        System.out.println("n = " + n);
+    }
+}
+
